9aac4eb3052c289f1ce6a90d627f1691ca865e16
martin nievas <martin.nievas.ar@gmail.com>
2018-12-17 19:53:37 -0300

    function for retrieve a random number, given the upper limit

--- programa.c
+++ programa.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int random_number(int upper_limit){
+  
+  int temp;
+
+  temp = rand() % upper_limit;
+
+  return temp;
+
+}
+
 int main(void)
 {
-  printf("%d\n", rand()%10);
+
+  int num;
+
+  num = random_number(100);
+
+  printf("%d\n", num);
 
   return 0;
 }
+
+
+
